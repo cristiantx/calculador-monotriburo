b966007c798b35798827c953916cb87482095bd5
Cristian Conedera <cristian.conedera@gmail.com>
2024-04-30 12:21:50 -0300

    arreglado error

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
-const fs = require('fs');
-const util = require('util');
-const xml2js = require('xml2js');
+const fs = require("fs");
+const util = require("util");
+const xml2js = require("xml2js");
 const parse = util.promisify(new xml2js.Parser().parseString);
 const read = util.promisify(fs.readFile);
-const Table = require('cli-table');
+const Table = require("cli-table");
+const createCSVFile = require("csv-file-creator");
 
 let facturas = [];
-let filePromises = []; 
+let filePromises = [];
 let grandTotal = 0;
+let dataCsv = [];
 
 var table = new Table({
-    head: ['Fecha', 'Monto', 'Cambio', 'Total']
-  , colWidths: [20, 20, 20, 20]
+  head: ["Fecha", "Monto", "Cambio", "Total"],
+  colWidths: [20, 20, 20, 20],
 });
 
-fs.readdir('./facturas', (err, files) => {
-    files.filter(file => file.endsWith('xml')).forEach(file => {
-        filePromises.push(
-            read(__dirname + '/facturas/' + file)
-            .then((data) => {
-                    return parse(data); 
-                })
-            .then((result) => {
-                const f = result.comprobante;
-                const data = [
-                    f.fechaemision[0],
-                    f.importetotal[0], 
-                    f.tipocambio[0], 
-                    f.importetotal[0] * f.tipocambio[0]
-                ];
-                grandTotal = grandTotal + f.importetotal[0] * f.tipocambio[0];
-                table.push(data);
-                return true;
-            })
-        );
+fs.readdir("./facturas", (err, files) => {
+  files
+    .filter((file) => file.endsWith("xml"))
+    .forEach((file) => {
+      filePromises.push(
+        read(__dirname + "/facturas/" + file)
+          .then((data) => {
+            return parse(data);
+          })
+          .then((result) => {
+            const f = result.comprobante;
+            console.log("[debug]", f);
+
+            const tipoCambio =
+              f.tipocambio && f.tipocambio[0] ? f.tipocambio[0] : 1;
+            const data = [
+              f.fechaemision[0],
+              f.importetotal[0],
+              tipoCambio,
+              f.importetotal[0] * tipoCambio,
+            ];
+            dataCsv.push(data);
+            grandTotal = grandTotal + f.importetotal[0] * tipoCambio;
+            table.push(data);
+            return true;
+          })
+      );
     });
-    Promise.all(filePromises).then(() => {
-        console.log(table.toString());
-        console.log("\nTotal Facturado: $"+grandTotal);
-    });
-})
+  Promise.all(filePromises).then(() => {
+    createCSVFile("facturacion-monotributo.csv", dataCsv);
+
+    console.log(
+      table
+        .sort((a, b) => {
+          if (new Date(a[0]) > new Date(b[0])) {
+            return 1;
+          } else {
+            return -1;
+          }
+        })
+        .toString()
+    );
+    console.log("\nTotal Facturado: $" + grandTotal);
+  });
+});
